0c12a81d4a6bc028879154fff89be58fe930f694
Sanmuell <sanmuell.i@gmail.com>
2022-01-13 14:07:42 -0300

    first commit

---
+++ src/test/java/br/com/gestorfinanceiroapi/GestorFinanceiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestorfinanceiroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorFinanceiroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

be342589725b0418f3053edd35e1007fa4c5847a
Sanmuell <sanmuell.i@gmail.com>
2022-01-15 09:49:18 -0300

    delete resource

--- src/main/java/br/com/gestorfinanceiroapi/resources/CategoriaResource.java
+++ src/main/java/br/com/gestorfinanceiroapi/resources/CategoriaResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,4 +65,9 @@ public class CategoriaResource {
 
 	}
 
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		categoriaRepository.deleteById(codigo);
+	}
 }
--- src/main/java/br/com/gestorfinanceiroapi/resources/PessoaResource.java
+++ src/main/java/br/com/gestorfinanceiroapi/resources/PessoaResource.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gestorfinanceiroapi.event.RecursoCriadoEvent;
@@ -24,42 +26,47 @@ import br.com.gestorfinanceiroapi.repositories.PessoaRepository;
 @RestController
 @RequestMapping(path = "/pessoas")
 public class PessoaResource {
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
 	@GetMapping
-	public  List<Pessoa> listarTodas(){
+	public List<Pessoa> listarTodas() {
 		return pessoaRepository.findAll();
-		
+
 	}
-	
+
 	@GetMapping("/{codigo}")
-	public ResponseEntity<Optional<Pessoa>> listarPorId (@PathVariable Long codigo){
-		
+	public ResponseEntity<Optional<Pessoa>> listarPorId(@PathVariable Long codigo) {
+
 		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
-		
+
 		if (pessoa.isPresent()) {
-			
+
 			return ResponseEntity.ok().body(pessoa);
 		}
-		
+
 		return ResponseEntity.notFound().build();
-		
+
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Pessoa> salvar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
-	Pessoa pessoaSalva = pessoaRepository.save(pessoa);
-	
-	publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
-	
-	return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
-		
+	public ResponseEntity<Pessoa> salvar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
+
 	}
-	
-	
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
+	}
+
 }

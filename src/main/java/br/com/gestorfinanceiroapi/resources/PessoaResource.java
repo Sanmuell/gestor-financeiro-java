792f4b78f187ccab7d52972abbd91943133e0f13
Sanmuell <sanmuell.i@gmail.com>
2022-01-16 12:00:22 -0300

    put endpoint

--- src/main/java/br/com/gestorfinanceiroapi/resources/PessoaResource.java
+++ src/main/java/br/com/gestorfinanceiroapi/resources/PessoaResource.java
@@ -6,14 +6,17 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.gestorfinanceiroapi.event.RecursoCriadoEvent;
 import br.com.gestorfinanceiroapi.models.Pessoa;
 import br.com.gestorfinanceiroapi.repositories.PessoaRepository;
+import br.com.gestorfinanceiroapi.service.PessoaService;
 
 @RestController
 @RequestMapping(path = "/pessoas")
@@ -29,6 +33,9 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private PessoaService pessoaService;
 
 	@Autowired
 	private ApplicationEventPublisher publisher;
@@ -68,5 +75,22 @@ public class PessoaResource {
 	public void remover(@PathVariable Long codigo) {
 		pessoaRepository.deleteById(codigo);
 	}
+	
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa){
+		Pessoa pessoaAtual = pessoaService.atualizar(codigo, pessoa);
+		return ResponseEntity.ok(pessoaAtual);
+		
+	}
+	
+	@PutMapping("/{codigo}/ativo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizarPropriedadeAtivo (@PathVariable Long codigo, @RequestBody Boolean ativo) {
+		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+		
+		
+	}
+	
 
 }
---
+++ src/main/java/br/com/gestorfinanceiroapi/service/PessoaService.java
@@ -0,0 +1,42 @@
+package br.com.gestorfinanceiroapi.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import br.com.gestorfinanceiroapi.models.Pessoa;
+import br.com.gestorfinanceiroapi.repositories.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Pessoa atualizar(@PathVariable Long codigo, @RequestBody Pessoa pessoa) {
+
+		Pessoa pessoaSalva = encontrarPessoaPeloCodigo(codigo);
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return this.pessoaRepository.save(pessoaSalva);
+		
+
+	}
+
+	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+
+		Pessoa pessoaSalva = encontrarPessoaPeloCodigo(codigo);
+		pessoaSalva.setAtivo(ativo);
+		pessoaRepository.save(pessoaSalva);
+
+	}
+
+	private Pessoa encontrarPessoaPeloCodigo(Long codigo) {
+		Pessoa pessoaSalva = this.pessoaRepository.findById(codigo)
+				.orElseThrow(() -> new EmptyResultDataAccessException(1));
+		return pessoaSalva;
+	}
+
+}
